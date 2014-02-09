2710269d301df30558e6ad1b5fe6b2018e1899c4
textfield <textfield@9dcbdd57-5c4c-0410-be14-8976a164caa6>
2014-02-09 15:55:22 +0000

    Dates.

--- use.c
+++ use.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2006, 2007, 2013, 2014 Alexis Megas
+** Copyright (c) 2006 - Eternity Alexis Megas
 ** All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without
